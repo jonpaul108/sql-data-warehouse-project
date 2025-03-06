53dabfe66d8c9bc0cc9a19d236a94ce26d38c752
Jonathan McNamara <42330059+jonpaul108@users.noreply.github.com>
2025-03-05 21:15:15 -0800

    add erp_px_cat_g1v2 load query

--- scripts/silver/cleans_and_load_silver.sql
+++ scripts/silver/cleans_and_load_silver.sql
@@ -131,3 +131,16 @@ SELECT
     ELSE TRIM(cntry)
   END AS cntry
 FROM bronze.erp_loc_a101;
+
+INSERT INTO silver.erp_px_cat_g1v2(
+  id,
+  cat,
+  subcat,
+  maintenance
+)
+SELECT 
+id,
+cat,
+subcat,
+ TRIM(REPLACE(maintenance,CHAR(13),'')) as maintenance
+ FROM bronze.erp_px_cat_g1v2;
