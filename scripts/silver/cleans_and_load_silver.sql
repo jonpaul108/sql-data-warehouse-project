ad05516ad9702f38c175316d87d2d049c46b6682
Jonathan McNamara <42330059+jonpaul108@users.noreply.github.com>
2025-03-05 17:04:44 -0800

    Create cleans_and_load_silver.sql

---
+++ scripts/silver/cleans_and_load_silver.sql
@@ -0,0 +1,96 @@
+INSERT INTO silver.crm_cust_info (
+  cst_id,
+  cst_key,
+  cst_firstname,
+  cst_lastname,
+  cst_marital_status,
+  cst_gndr,
+  cst_created_dt
+)
+SELECT
+  cst_id,
+  cst_key,
+  TRIM(cs_firstname) AS cst_firstname,
+  TRIM(cs_lastname) AS cst_lastname,
+  cst_marital_status,
+  CASE 
+    WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'MALE'
+    WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'FEMALE'
+    ELSE 'N/A'
+  END AS cst_gndr,
+  cst_created_dt
+FROM 
+(
+SELECT 
+  *,
+  CASE 
+    WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'MARRIED'
+    WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'SINGLE'
+    ELSE 'N/A'
+  END AS cst_marital_status,
+  ROW_NUMBER() OVER (partition by cst_id ORDER BY cst_created_dt DESC) AS flag_last
+FROM 
+  bronze.crm_cust_info
+WHERE cst_id IS NOT NULL
+) t WHERE flag_last = 1;
+
+INSERT INTO silver.crm_prd_info (
+   prd_id,
+  cat_id,
+  prd_key,
+  prd_nm ,
+  prd_cost,
+  prd_line,
+  prd_start_dt,
+  prd_end_dt
+)
+SELECT
+  prd_id,
+  REPLACE(left(prd_key, 5), '-', '_') as cat_key,
+  SUBSTRING(prd_key, 7, LEN(prd_key)) as prd_key,
+  prd_nm,
+  ISNULL(prd_cost, 0) as prd_cost,
+  CASE UPPER(TRIM(prd_line))
+    WHEN 'M' THEN 'MOUNTAIN'
+    WHEN 'R' THEN 'ROAD'
+    WHEN 'T' THEN 'Touring'
+    WHEN 'S' THEN 'other Sales'
+    ELSE 'n/a' 
+  END as prd_line,
+  prd_start_dt,
+  DATEADD(DAY, -1, LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)) AS prd_end_dt_test
+FROM
+bronze.crm_prd_info;
+
+INSERT INTO silver.crm_sales_details (
+  sls_ord_num,
+  sls_prd_key,
+  sls_cust_id,
+  sls_order_dt,
+  sls_ship_dt,
+  sls_due_dt,
+  sls_sales,
+  sls_quantity,
+  sls_price
+)
+SELECT 
+  sls_ord_num,
+  sls_prd_key,
+  sls_cust_id,
+  CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL 
+    ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) END AS sls_order_dt,
+  CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL 
+    ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) END AS sls_ship_dt,
+  CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL 
+    ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) END AS sls_due_dt,
+    CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+    THEN sls_quantity * ABS(sls_price)
+    ELSE sls_sales
+  END AS sls_sales,
+  sls_quantity,
+  CASE WHEN sls_price IS NULL OR sls_price <= 0 
+    THEN sls_sales / NULLIF(sls_quantity, 0) 
+    ELSE sls_price
+  END AS sls_price
+FROM 
+bronze.crm_sales_details;
