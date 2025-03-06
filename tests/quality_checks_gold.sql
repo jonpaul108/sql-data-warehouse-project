a349a2454fef7f49745a2d85a5d563c7aa52a29c
Jonathan McNamara <42330059+jonpaul108@users.noreply.github.com>
2025-03-06 14:20:57 -0800

    Update quality_checks_gold.sql
    add fact view check

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -8,3 +8,14 @@ Gold Layer Checks
 SELECT DISTINCT
 gender
 FROM gold.dim_customers;
+
+-- check fact correctly joins with gold.dim_customers and gold.dum_product, expectation: nothing returned
+SELECT 
+* 
+FROM 
+gold.fact_sales f 
+LEFT JOIN gold.dim_customers cu
+ON cu.customer_key = f.customer_key
+LEFT JOIN gold.dim_product pr
+ON pr.product_key = f.product_key
+WHERE cu.customer_key IS NULL;
