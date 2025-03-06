a8c1ab862c66240079a34fd940e7e24f48a0e74c
Jonathan McNamara <42330059+jonpaul108@users.noreply.github.com>
2025-03-06 14:14:25 -0800

    add create fact view query

--- scripts/gold/create_gold_layer_views.sql
+++ scripts/gold/create_gold_layer_views.sql
@@ -50,3 +50,24 @@ CREATE VIEW gold.dim_product AS
   silver.crm_prd_info pi
   LEFT JOIN silver.erp_px_cat_g1v2 px ON pi.cat_id = px.id
 );
+
+
+-- create the sales fact view
+CREATE VIEW gold.fact_sales AS 
+(
+SELECT 
+  sls_ord_num AS order_number,
+  pr.product_key,
+  cu.customer_key,
+  sls_order_dt AS order_date,
+  sls_ship_dt AS shipping_date,
+  sls_due_dt AS due_date,
+  sls_sales as sales_amount,
+  sls_quantity AS quantity,
+  sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_product pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id
+);
